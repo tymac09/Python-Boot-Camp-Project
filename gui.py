7be7fb1a6087e595552d016f2148023ef6a5aa47
tymac09 <forward.run.9@gmail.com>
2024-12-16 14:56:02 -0500

    created a add button

--- gui.py
+++ gui.py
@@ -2,13 +2,32 @@ import functions
 import FreeSimpleGUI as sg
 
 lable = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
 
-window = sg.Window('My To-Do App', layout= [[lable], [input_box],[add_button]]) #layput is a argu that expect a list, item in the inner square bracket will be on the same row
+window = sg.Window('My To-Do App', 
+                   layout= [[lable], [input_box], [add_button] ], 
+                   font=('Helvetica', 20)
+                   
+                   )
+#layput is a argu that expect a list, item in the inner square bracket will be on the same row
 
-window.read()
+while True:
+        
+        event, values = window.read()
+        match event:
+                case "Add":
+                        todos = functions.get_todos()
+                        new_todo = values['todo'] + "\n"
+                        todos.append(new_todo)
+                        functions.write_todos(todos)
+                case sg.WIN_CLOSED:
+                        break
+                
+                        
+
+
 window.close()
 
  
